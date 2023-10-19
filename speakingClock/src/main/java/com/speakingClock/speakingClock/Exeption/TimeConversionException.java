fbecb3992ca948cfb6d436cc3803c48fa0518cc8
SURAJ <SURAJ@LAPTOP-IHMS8OC0>
2023-10-19 15:56:58 +0530

    speakingClockcommit

---
+++ speakingClock/src/main/java/com/speakingClock/speakingClock/Exeption/TimeConversionException.java
@@ -0,0 +1,8 @@
+package com.speakingClock.speakingClock.Exeption;
+
+public class TimeConversionException extends Exception {
+
+    public TimeConversionException(String message) {
+        super(message);
+    }
+}
---
+++ speakingClock/src/main/java/com/speakingClock/speakingClock/controller/TimeConversionController.java
@@ -0,0 +1,29 @@
+package com.speakingClock.speakingClock.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.speakingClock.speakingClock.Exeption.TimeConversionException;
+import com.speakingClock.speakingClock.service.TimeConversionService;
+
+@RestController
+public class TimeConversionController {
+
+    private final TimeConversionService timeConversionService;
+
+    public TimeConversionController(TimeConversionService timeConversionService) {
+        this.timeConversionService = timeConversionService;
+    }
+
+    @GetMapping("/convert-time")
+    public String convertToWords(@RequestParam String time) {
+        try {
+            String result = timeConversionService.convertToWords(time);
+            return result;
+        } catch (TimeConversionException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
+
